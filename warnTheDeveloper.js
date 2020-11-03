ee0b3c403a033297ea5ee15b15c0f74dcab59881
Moses Tosin <tosinmoses384@gmail.com>
2020-11-03 11:05:39 +0100

    first commit

---
+++ warnTheDeveloper.js
@@ -0,0 +1,28 @@
+ 
+function warnTheDeveloper(n, m, s) {
+    console.log((m-1 + s) % n) // the developer to warn  
+  }
+
+//   n: an integer, the number of developers
+// m: an integer, the number of sweets
+// s: an integer, the chair number to begin passing out sweets from
+
+//the developer to warn sit at chair number 2  
+  // warnTheDeveloper(3, 1, 2); 
+
+  //the developer to warn sit at chair number 2  
+  //warnTheDeveloper(5, 15, 1);
+
+  //the developer to warn sit at chair number 2
+  //warnTheDeveloper(5, 2, 1); 
+
+  //the developer to warn sit at chair number 3
+  //warnTheDeveloper(5, 2, 2); 
+
+  //the developer to warn sit at chair number 6
+  // warnTheDeveloper(7, 19, 2); 
+
+  //the developer to warn sit at chair number 3
+  // warnTheDeveloper(4, 6, 2); 
+  
+
